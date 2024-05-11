af5ebe1f4afefa4144cad6bfcc3438c124f9344a
Therushmorare <tshego.tt.morare@gmail.com>
2024-05-11 08:44:05 -0700

    task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,70 +1,52 @@
 import sha1 from 'sha1';
 import Queue from 'bull';
-import { findUserById, findUserIdByToken } from '../utils/helpers';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
+import getIdAndKey from '../utils/users';
 
-const userQueue = new Queue('userQueue');
+const userQ = new Queue('userQ');
 
 class UsersController {
-  /**
-   * Creates a user using email and password
-   */
-  static async postNew(request, response) {
-    const { email, password } = request.body;
+  static async postNew(req, res) {
+    const { email, password } = req.body;
 
-    // check for email and password
-    if (!email) return response.status(400).send({ error: 'Missing email' });
-    if (!password) return response.status(400).send({ error: 'Missing password' });
-
-    // check if the email already exists in DB
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
     const emailExists = await dbClient.users.findOne({ email });
-    if (emailExists) return response.status(400).send({ error: 'Already exist' });
+    if (emailExists) return res.status(400).send({ error: 'Already exist' });
 
-    // Insert new user
-    const sha1Password = sha1(password);
-    let result;
-    try {
-      result = await dbClient.users.insertOne({
-        email, password: sha1Password,
-      });
-    } catch (err) {
-      await userQueue.add({});
-      return response.status(500).send({ error: 'Error creating user' });
-    }
+    const secPass = sha1(password);
 
-    const user = {
-      id: result.insertedId,
+    const insertStat = await dbClient.users.insertOne({
+      email,
+      password: secPass,
+    });
+
+    const createdUser = {
+      id: insertStat.insertedId,
       email,
     };
 
-    await userQueue.add({
-      userId: result.insertedId.toString(),
+    await userQ.add({
+      userId: insertStat.insertedId.toString(),
     });
 
-    return response.status(201).send(user);
+    return res.status(201).send(createdUser);
   }
 
-  /**
-   * Should retrieve the user base on the token used
-   */
-  static async getMe(request, response) {
-    const token = request.headers['x-token'];
-    if (!token) { return response.status(401).json({ error: 'Unauthorized' }); }
+  static async getMe(req, res) {
+    const { userId } = await getIdAndKey(req);
 
-    // Retrieve the user based on the token
-    const userId = await findUserIdByToken(request);
-    if (!userId) return response.status(401).send({ error: 'Unauthorized' });
+    const user = await dbClient.users.findOne({ _id: ObjectId(userId) });
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
 
-    const user = await findUserById(userId);
+    const userInfo = { id: user._id, ...user };
+    delete userInfo._id;
+    delete userInfo.password;
 
-    if (!user) return response.status(401).send({ error: 'Unauthorized' });
-
-    const processedUser = { id: user._id, ...user };
-    delete processedUser._id;
-    delete processedUser.password;
-    // Return the user object (email and id only)
-    return response.status(200).send(processedUser);
+    return res.status(200).send(userInfo);
   }
 }
 
-module.exports = UsersController;
+export default UsersController;
+

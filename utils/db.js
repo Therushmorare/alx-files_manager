9e9836a491435463773520e551691c91e46eed28
Therushmorare <tshego.tt.morare@gmail.com>
2024-05-12 12:55:05 -0700

    tasks

--- utils/db.js
+++ utils/db.js
@@ -1,43 +1,36 @@
 import { MongoClient } from 'mongodb';
+// const { MongoClient } = require('mongodb');
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-    const url = `mongodb://${host}:${port}/${database}`;
-
-    this.client = new MongoClient(url, { useUnifiedTopology: true });
-
-    this.client.connect((err) => {
-      if (err) {
-        console.error(`DB connection error: ${err}`);
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        this.db = client.db(DB_DATABASE);
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
       } else {
-        console.log('DB connected successfully');
+        console.log(err.message);
+        this.db = false;
       }
     });
   }
 
-  isAlive() {
-    return !!this.client && this.client.isConnected();
-  }
+  isAlive() { return !!this.db; }
 
-  async nbUsers() {
-    const db = this.client.db();
-    const users = db.collection('users');
-    const count = await users.countDocuments();
-    return count;
-  }
+  async nbUsers() { return this.users.countDocuments(); }
 
-  async nbFiles() {
-    const db = this.client.db();
-    const files = db.collection('files');
-    const count = await files.countDocuments();
-    return count;
+  async nbFiles() { return this.files.countDocuments(); }
+
+  async getUser(query) {
+    const user = await this.db.collection('users').findOne(query);
+    return user;
   }
 }
 
 const dbClient = new DBClient();
-
-export default dbClient;
-
+module.exports = dbClient;

d15293dce372ee6e00d08fb5c807abf091518464
Therushmorare <tshego.tt.morare@gmail.com>
2024-05-12 12:29:19 -0700

    tasks

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,52 +1,70 @@
 import sha1 from 'sha1';
 import Queue from 'bull';
-import { ObjectId } from 'mongodb';
+import { findUserById, findUserIdByToken } from '../utils/helpers';
 import dbClient from '../utils/db';
-import getIdAndKey from '../utils/users';
 
-const userQ = new Queue('userQ');
+const userQueue = new Queue('userQueue');
 
 class UsersController {
-  static async postNew(req, res) {
-    const { email, password } = req.body;
+  /**
+   * Creates a user using email and password
+   */
+  static async postNew(request, response) {
+    const { email, password } = request.body;
 
-    if (!email) return res.status(400).send({ error: 'Missing email' });
-    if (!password) return res.status(400).send({ error: 'Missing password' });
+    // check for email and password
+    if (!email) return response.status(400).send({ error: 'Missing email' });
+    if (!password) return response.status(400).send({ error: 'Missing password' });
+
+    // check if the email already exists in DB
     const emailExists = await dbClient.users.findOne({ email });
-    if (emailExists) return res.status(400).send({ error: 'Already exist' });
+    if (emailExists) return response.status(400).send({ error: 'Already exist' });
 
-    const secPass = sha1(password);
+    // Insert new user
+    const sha1Password = sha1(password);
+    let result;
+    try {
+      result = await dbClient.users.insertOne({
+        email, password: sha1Password,
+      });
+    } catch (err) {
+      await userQueue.add({});
+      return response.status(500).send({ error: 'Error creating user' });
+    }
 
-    const insertStat = await dbClient.users.insertOne({
-      email,
-      password: secPass,
-    });
-
-    const createdUser = {
-      id: insertStat.insertedId,
+    const user = {
+      id: result.insertedId,
       email,
     };
 
-    await userQ.add({
-      userId: insertStat.insertedId.toString(),
+    await userQueue.add({
+      userId: result.insertedId.toString(),
     });
 
-    return res.status(201).send(createdUser);
+    return response.status(201).send(user);
   }
 
-  static async getMe(req, res) {
-    const { userId } = await getIdAndKey(req);
+  /**
+   * Should retrieve the user base on the token used
+   */
+  static async getMe(request, response) {
+    const token = request.headers['x-token'];
+    if (!token) { return response.status(401).json({ error: 'Unauthorized' }); }
 
-    const user = await dbClient.users.findOne({ _id: ObjectId(userId) });
-    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+    // Retrieve the user based on the token
+    const userId = await findUserIdByToken(request);
+    if (!userId) return response.status(401).send({ error: 'Unauthorized' });
 
-    const userInfo = { id: user._id, ...user };
-    delete userInfo._id;
-    delete userInfo.password;
+    const user = await findUserById(userId);
 
-    return res.status(200).send(userInfo);
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
+
+    const processedUser = { id: user._id, ...user };
+    delete processedUser._id;
+    delete processedUser.password;
+    // Return the user object (email and id only)
+    return response.status(200).send(processedUser);
   }
 }
 
-export default UsersController;
-
+module.exports = UsersController;
--- utils/db.js
+++ utils/db.js
@@ -1,36 +1,43 @@
 import { MongoClient } from 'mongodb';
-// const { MongoClient } = require('mongodb');
-
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
-const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
 class DBClient {
   constructor() {
-    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-      if (!err) {
-        this.db = client.db(DB_DATABASE);
-        this.users = this.db.collection('users');
-        this.files = this.db.collection('files');
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+
+    this.client.connect((err) => {
+      if (err) {
+        console.error(`DB connection error: ${err}`);
       } else {
-        console.log(err.message);
-        this.db = false;
+        console.log('DB connected successfully');
       }
     });
   }
 
-  isAlive() { return !!this.db; }
+  isAlive() {
+    return !!this.client && this.client.isConnected();
+  }
 
-  async nbUsers() { return this.users.countDocuments(); }
+  async nbUsers() {
+    const db = this.client.db();
+    const users = db.collection('users');
+    const count = await users.countDocuments();
+    return count;
+  }
 
-  async nbFiles() { return this.files.countDocuments(); }
-
-  async getUser(query) {
-    const user = await this.db.collection('users').findOne(query);
-    return user;
+  async nbFiles() {
+    const db = this.client.db();
+    const files = db.collection('files');
+    const count = await files.countDocuments();
+    return count;
   }
 }
 
 const dbClient = new DBClient();
-module.exports = dbClient;
+
+export default dbClient;
+
